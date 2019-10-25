40ba649414e59848c6afe12b097a4aad6819adad
dfavo <favorskyd@gmail.com>
2019-10-25 13:31:42 -0700

    Add new files

---
+++ test/specs/constants.js
@@ -0,0 +1,28 @@
+const baseUrl = "https://stage.pasv.us";
+
+const url = {
+  baseUrl,
+
+  login: `${baseUrl}/user/login`,
+  register: `${baseUrl}/user/register`,
+  forgotPassword: `${baseUrl}/user/password/reset/request`,
+  checkEmail: `${baseUrl}/user/password/reset/mailed`,
+
+  diaryList: `${baseUrl}/diary`,
+  diaryCreateForm: `${baseUrl}/diary/create`,
+
+  group: `${baseUrl}/group`,
+  createGroup: `${baseUrl}/group/create`,
+
+  server: "https://server-stage.pasv.us"
+};
+
+const app = {
+  name: "Progress Monitor",
+  slogan: "eat(); sleep(); code(); repeat();"
+};
+
+module.exports = {
+  url,
+  app
+};
---
+++ test/specs/login.js
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+
+describe("Login form", () => {
+  before(() => {
+    browser.url("https://stage.pasv.us/user/login");
+  });
+
+  it("should login", () => {});
+});
